9ee21fa49336eaae44f4a2474fb4795cf8cbea96
Devbriandurand <durand.brian33@gmail.com>
2020-06-02 00:57:35 +0200

    v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-angular';
+
+export class AppComponent implements OnInit {
+
+  isAuth = false;//Variable pour savoir si l'utilisateur est identifier par défaul = faux
+
+  heureActuel = new Promise((resolve, reject) => {//PIPES ASYNC AVEC PROMISE [OBJET AVEC PROMISE]
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+
+  appareils : any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(() => this.isAuth = true, 4000);
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllume() {
+    this.appareilService.switchOnAll();
+  }
+
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
 }
